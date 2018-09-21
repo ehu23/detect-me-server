10512074b723e197cb4dc6dc7a5641d2b3bbdab0
Eddie Hu <ehu23@users.noreply.github.com>
2018-09-21 04:42:16 -0700

    includes README and uses the PORT environmental variable to dynamically set the port for the server

---
+++ README.md
@@ -0,0 +1,3 @@
+README
+
+Default npm start script runs the server using nodemon on port 3001 through bash. To change the port, use the environment variable PORT.
--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)}); //
 app.put('/image', (req, res) => {image.handleImage(req, res, db)}); 
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)}); 
 
-app.listen(3001, () => {
-    console.log('app is running on port 3001');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 });
