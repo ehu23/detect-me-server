6bd5774af38feebea7f7b95912506f800423abc5
Eddie Hu <ehu23@users.noreply.github.com>
2018-09-20 23:32:06 -0700

    basic register functionality works with postgres database

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',  //127.0.0.1 is localhost
       user : 'eddie',
-      password : '',
+      password : '***REMOVED***',
       database : 'smart-brain'
     }
   });
 
-console.log(postgres.select('*').from('users'));
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -62,15 +64,16 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body; 
-    
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0, 
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length-1])
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+    }).then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('unable to register')) //in case theres an error
 });
 
 app.get('/profile/:id', (req, res) => { //not implemented w/ frontend
